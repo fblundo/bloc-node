cbcc99db7e5ecd96202e238ff94655ee06faa0ee
Blundo <federico.blundo@bain.com>
2019-09-17 22:31:24 +0200

    Greetings assignment completed

---
+++ Greetings/main.js
@@ -0,0 +1,12 @@
+const greetings = require("./greeting.js");
+const seeyoulater = require("./seeyoulater.js");
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingIcelandic();
+greetings.greetingSpanish();
+
+seeyoulater.seeyoulaterEnglish();
+seeyoulater.seeyoulaterJapanese();
+seeyoulater.seeyoulaterIcelandic();
+seeyoulater.seeyoulaterSpanish();
---
+++ Greetings/seeyoulater.js
@@ -0,0 +1,15 @@
+exports.seeyoulaterEnglish = function(){
+  console.log('See you later');
+}
+
+exports.seeyoulaterJapanese = function(){
+  console.log('さよら');
+}
+
+exports.seeyoulaterSpanish = function(){
+  console.log('A despures');
+}
+
+exports.seeyoulaterIcelandic = function(){
+  console.log('Bless');
+}
